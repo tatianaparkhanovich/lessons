ca04a913113eb42efc7b62c7b939f854dcc9f627
tatianaparkhanovich <tania.tigrovaa@gmail.com>
2023-08-28 22:22:46 +0300

    inquiries html,js

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const requestURL = 'https://jsonplaceholder.typicode.com/todos/1';
 const xhr = new XMLHttpRequest();
 xhr.open('GET', requestURL);
 xhr.onload = () => {
-    const response = JSON.parse (xhr.response);
-    root.innerHTML += response.title;
+    if (xhr.status >= 200 && xhr.status < 300) {
+        const response = JSON.parse (xhr.response);
+        root.innerHTML += response.title;
+    } else {
+        console.error('Ошибка при выполнении запроса', xhr.status);
+    }
 }
 xhr.send();
 
@@ -20,14 +24,18 @@ const request = 'https://jsonplaceholder.typicode.com/todos';
 const xttp = new XMLHttpRequest();
 xttp.open('GET', request);
 xttp.onload = () => {
-    const ul = document.createElement('ul');
-    document.body.appendChild(ul);
-    const response = JSON.parse(xttp.response);
-    const slise = response.slice(0, 20);
-    slise.forEach(element => {
-        const li = document.createElement('li');
-        ul.append(li);
-        li.innerHTML += element.title; 
-    });
+    if (xttp.status >= 200 && xttp.status < 300) {
+        const ul = document.createElement('ul');
+        document.body.appendChild(ul);
+        const response = JSON.parse(xttp.response);
+        const slise = response.slice(0, 20);
+        slise.forEach(element => {
+            const li = document.createElement('li');
+            ul.append(li);
+            li.innerHTML += element.title;
+        })
+    } else {
+        console.error('Ошибка при выполнении запроса', xttp.status);
+    }
 }
 xttp.send();
